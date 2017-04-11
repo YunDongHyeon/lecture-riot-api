b488fcb62a5738fd0c9c51d5759fe3272a2dbc8c
YunDongHyeon <ydh2338@naver.com>
2017-04-11 16:38:15 +0900

    update

---
+++ src/main/java/univ/lecture/riotapi/model/CalcApp.java
@@ -0,0 +1,34 @@
+package univ.lecture.riotapi.model;
+
+import java.util.Arrays;
+import java.util.logging.*;
+
+/**
+ * Calculator application
+ */
+public class CalcApp {
+    public double calc(String[] tokens) {
+        final double firstOperand;
+        final double secondOperand;
+        firstOperand = Double.parseDouble(tokens[0]);
+        if (tokens.length > 2) {
+            secondOperand = Double.parseDouble(tokens[2]);
+        } else {
+            secondOperand = Double.parseDouble(tokens[1]);
+        }
+        final Operator operator = Operator.findOperator(tokens[1]);
+
+        return operator.evaluate(firstOperand, secondOperand);
+
+    }
+
+    public static void main( String[] args ) {
+        final CalcApp app = new CalcApp();
+        final StringBuilder outputs = new StringBuilder();
+        Arrays.asList(args).forEach(value -> outputs.append(value + " "));
+
+	Logger logger =Logger.getLogger("Pring Result");
+	String result="Addtition of values"+outputs+"="+Double.toString(app.calc(args));
+	logger.log(Level.INFO, result);
+    }
+}
